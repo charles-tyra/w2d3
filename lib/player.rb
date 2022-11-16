4404304ef5fd7a18bcf39b3afbfb5f0a5a24994d
Charles Tyra <charlestyra@macbook-pro.myfiosgateway.com>
2022-11-16 16:27:44 -0500

    Player Class, contains #get_move method

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,7 @@
 class Player
-
+    def get_move
+        print "enter a position with coordinates separated with a space like '4 7'"
+        move = gets.chomp
+        move_position = [move[0].to_i, move[2].to_i]
+    end
 end
