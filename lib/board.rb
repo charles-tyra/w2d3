36a801d6b03302fbd8ae2b2fdbe61a974b9689fa
Charles Tyra <charlestyra@macbook-pro.myfiosgateway.com>
2022-11-16 14:38:35 -0500

    Board Part 1 completion, contains #size, #[position], #[position]=value, num_ships methods

--- lib/board.rb
+++ lib/board.rb
@@ -4,4 +4,20 @@ class Board
     @grid = Array.new(n) { Array.new(n, :N) }
     @size = n * n
   end
+
+  def size
+    @size
+  end
+
+  def [](pos)
+    @grid[pos[0]][pos[1]]
+  end
+
+  def []=(pos, value)
+    @grid[pos[0]][pos[1]] = value
+  end
+
+  def num_ships
+    @grid.flatten.count { |ele| ele == :S }
+  end
 end
