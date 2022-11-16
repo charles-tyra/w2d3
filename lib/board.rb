74a1c6bde05b817ee306aa892c48cd1cb0459e08
Charles Tyra <charlestyra@macbook-pro.myfiosgateway.com>
2022-11-16 15:09:34 -0500

    Part 2 of Battleship Project, contains #attack and #place_random_ships, more to come from Part 2

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,5 @@
 class Board
+    #Part 1
   def initialize(n)
     @n = n
     @grid = Array.new(n) { Array.new(n, :N) }
@@ -19,5 +20,34 @@ class Board
 
   def num_ships
     @grid.flatten.count { |ele| ele == :S }
+  end
+
+  #Part 2
+  def attack(pos)
+    if self[pos] == :S
+        self[pos] = :H
+        print "you sunk my battleship!"
+        true
+    else
+        self[pos] = :X
+        false
+    end
+  end
+
+  def place_random_ships
+    total_ships = size/4
+
+    count = 0
+    while count < total_ships
+        rand_num = rand(@size)
+        rand_row = rand_num / @n
+        rand_col = rand_num % @n
+        pos = [rand_row, rand_col]
+
+        if self[pos] == :N
+            count += 1
+            self[pos] = :S
+        end
+    end
   end
 end
